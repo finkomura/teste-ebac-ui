40d0fd981f2cf2326725cc36270d57a129947cc7
vitoria <finkedificacoes@gmail.com>
2023-10-04 22:04:17 +0100

    page object com massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types ="cypress"/>
 import EnderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade enterecos - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,8 +10,25 @@ describe('Funcionalidade enterecos - Faturamento e Entrega', () => {
             cy.login(dados.usuario, dados.senha)
         })
     });
-    it.only('deve fazer cadastro de faturamento com sucesso', () => {
-        EnderecoPage.editarEnderecoFaturamento()
+    it('deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('vi','blabla','essai','Brasil','local','101','barcarena', 'minas gerais','10010-111','911119999','blablabla@loll.com')
+        cy.get('.woocommerce-message').should('contain','alterado com sucesso.')
+    });
+
+    it.only('deve fazer cadastro de faturamento com sucesso - usando massa de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
         cy.get('.woocommerce-message').should('contain','alterado com sucesso.')
     });
 });
--- cypress/support/page-objects/endereco.page.js
+++ cypress/support/page-objects/endereco.page.js
@@ -1,20 +1,20 @@
 class EnderecoPAge {
 
-    editarEnderecoFaturamento() {
+    editarEnderecoFaturamento(nome, sobrenome,empresa, pais, endereco, numero, cidade, estado, cep, telefone, email) {
         cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a'). click()
         cy.get(':nth-child(1) > .title > .edit').click()
-        cy.get('#billing_first_name').clear().type('vicky')
-        cy.get('#billing_last_name').clear().type('omura')
-        cy.get('#billing_company').clear().type ('ebac')
+        cy.get('#billing_first_name').clear().type(nome)
+        cy.get('#billing_last_name').clear().type(sobrenome)
+        cy.get('#billing_company').clear().type (empresa)
 
-        cy.get('#select2-billing_country-container').click(). type('Brasil').get('[aria-selected="true"]').click()
-        cy.get('#billing_address_1').clear().type('Av sao joao')
-        cy.get('#billing_address_1').clear().type(1001)
-        cy.get('#billing_city').clear().type('Pindamoiangaba')
-        cy.get('#select2-billing_state-container').click().type('Rio de Janeiro{enter}')
-        cy.get('#billing_postcode').clear().type('10022-125')
-        cy.get('#billing_phone').clear().type('996003535')
-        cy.get('#billing_email').clear().type('teste@teste.ebac')
+        cy.get('#select2-billing_country-container').click(). type(pais).get('[aria-selected="true"]').click()
+        cy.get('#billing_address_1').clear().type(endereco)
+        cy.get('#billing_address_1').clear().type(numero)
+        cy.get('#billing_city').clear().type(cidade)
+        cy.get('#select2-billing_state-container').click().type(estado+'{Enter}')
+        cy.get('#billing_postcode').clear().type(cep)
+        cy.get('#billing_phone').clear().type(telefone)
+        cy.get('#billing_email').clear().type(email)
         cy.get(':nth-child(2) > .button').click()
     }
     editarEnderecoEntrega() {
